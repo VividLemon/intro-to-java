b78ecb98d6174396b1e671e0ff71122513c35d88
Issayah C <copseyi@students.westerntc.edu>
2020-01-23 13:53:54 -0600

    InputAndOutput finished

---
+++ InputAndOutput.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class InputAndOutput{
+
+    public static void main(String[] args){
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Enter some input!");
+        String input = keyboard.next();
+        System.out.println("Here is your input as output: " + input);
+    }
+
+}
