ac440ea2707df863f7b0654e1d6c05242190190a
Issayah C <copseyi@students.westerntc.edu>
2020-01-28 14:38:50 -0600

    constants and scannerdemo added

---
+++ Java Workspace/Java Sample Code/src/com/issayah/samplecode/Constants.java
@@ -0,0 +1,14 @@
+package com.issayah.samplecode;
+
+public class Constants {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		final int MAX_AGE = 130;
+		final int MIN_AGE = 0;
+			
+			System.out.println("You can not be younger than " + MIN_AGE);
+	}
+
+}
